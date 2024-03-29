aeb5ee92c52e6cb03ce15094dd9e404c01b584c5
1world 2geeks <1world2geeks@gmail.com>
2024-03-29 14:37:21 -0300

    Agregamos en el maquetado productInfoFigure debajo de ProductInfo, se agregaron nuevos productos al array

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+//Asignamos la selección de elementos HTML a una Constante.
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
 const menuCarritoIcon = document.querySelector('.navbar-shopping-cart');
@@ -6,11 +7,13 @@ const mobilMenu = document.querySelector('.mobile-menu');
 const aside = document.querySelector('.product-detail');
 const cardsContainer = document.querySelector('.cards-container');
 
+//Escuchamos el evento click para llamar una función
 menuEmail.addEventListener('click', toggleDesktopMenu);
 iconMenu.addEventListener('click', toggleMobilMenu);
 menuCarritoIcon.addEventListener('click', toggleCarritoAside);
 
 function toggleDesktopMenu(){
+    //Si existe devolvera un True
     const isAsideClosed = aside.classList.contains('inactive');
 
     //Verificamos que el aside este abierto
@@ -66,32 +69,64 @@ productList.push({
 });
 
 productList.push({
-    name: 'Pantalla',
-    price: 500,
-    imagen: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+    name: 'Moto BMW - Coffe Reacer',
+    price: 5000,
+    imagen: 'https://cdn.pixabay.com/photo/2016/04/07/06/53/bmw-1313343_960_720.jpg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
 });
 
 productList.push({
-    name: 'PC',
+    name: 'GameBoy',
     price: 800,
-    imagen: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+    imagen: 'https://images.unsplash.com/photo-1531525645387-7f14be1bdbbd?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
 });
 
+productList.push({
+    name: 'TOYOTA MR2',
+    price: 7000,
+    imagen: 'https://images.pexels.com/photos/17797237/pexels-photo-17797237/free-photo-of-coche-vehiculo-vintage-retro.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+
+})
+
+//
+productList.push({
+    name: 'Cassete Video',
+    price: 80,
+    imagen: 'https://images.pexels.com/photos/17758160/pexels-photo-17758160/free-photo-of-sucio-vintage-retro-kodak.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+})
 
 
-//    <div class="product-card">
-//       <img src="https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="">
-//       <div class="product-info">
-//         <div>
-//           <p>$120,00</p>
-//           <p>Bike</p>
-//         </div>
-//         <figure>
-//           <img src="./icons/bt_add_to_cart.svg" alt="">
-//         </figure>
-//       </div>
-//     </div>
 
+//6
+productList.push({
+    name: 'Telefono',
+    price: 120,
+    imagen: 'https://images.pexels.com/photos/17786712/pexels-photo-17786712/free-photo-of-madera-vintage-de-madera-antiguo.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+
+})
+
+//7
+productList.push({
+    name: 'Wold Waggen Band',
+    price: 7000,
+    imagen: 'https://images.pexels.com/photos/17606859/pexels-photo-17606859/free-photo-of-vehiculo-viaje-viajar-retro.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+
+})
+
+//8
+productList.push({
+    name: 'Motor - V5 ',
+    price: 7000,
+    imagen: 'https://images.pexels.com/photos/17575475/pexels-photo-17575475/free-photo-of-coche-vehiculo-tuberias-retro.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+
+})
+
+//9
+productList.push({
+    name: 'Write Machine',
+    price: 600,
+    imagen: 'https://images.pexels.com/photos/3808904/pexels-photo-3808904.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+
+})
 
 //Resivimos un array como parametro para renderizar
 function renderProducts(arr){
@@ -136,6 +171,7 @@ function renderProducts(arr){
     
             productCard.appendChild(img);
             productCard.appendChild(productInfo);
+            
     
                 //Generamos el Orden de los DIV
                 productInfo.appendChild(productInfoDiv);
@@ -144,8 +180,10 @@ function renderProducts(arr){
                     productInfoDiv.appendChild(productPrice);
                     productInfoDiv.appendChild(productName);
     
+                productInfo.appendChild(productInfoFigure);
                     //Generamos el Orden de los DIV
                     productInfoFigure.appendChild(productInfoFigureImg);
+                    console.log(productInfoFigureImg);
     
     }
 
