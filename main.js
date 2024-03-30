3700e4ed1fa16bab6410e5370a90f58417b33319
1world 2geeks <1world2geeks@gmail.com>
2024-03-30 12:05:00 -0300

    Agregamos una descripción a cada elemento en los objtos de datos, se agrega un ID en el index.html para identificar el parrafo a modificar. Agregamos el codigo JS necesario para que aparesca la descripcion en el Aside Detail

--- main.js
+++ main.js
@@ -10,6 +10,7 @@ const productDetailContainer = document.querySelector('#productDetail');
 const imagenDetailID = document.querySelector('#imagenDetail');
 const priceDetail = document.querySelector('#priceDetail');
 const nameDetail = document.querySelector('#nameDetail');
+const descripcionDetail = document.querySelector('#descripcionDetail');
 
 
 const cardsContainer = document.querySelector('.cards-container');
@@ -96,39 +97,44 @@ productList.push({
     name: 'bike',
     price: 120,
     imagen: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+    descripcion: 'Bicicleta super impresionante con velocidades incomparables, vintage modelo Peugeot',
 });
 
 productList.push({
     name: 'Moto BMW - Coffe Reacer',
     price: 5000,
     imagen: 'https://cdn.pixabay.com/photo/2016/04/07/06/53/bmw-1313343_960_720.jpg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+    descripcion: 'Una gran moto de la marca BMW custom, modelado a una clasica apariencia Coffee Reacer',
 });
 
 productList.push({
     name: 'GameBoy',
     price: 800,
     imagen: 'https://images.unsplash.com/photo-1531525645387-7f14be1bdbbd?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+    descripcion: 'Una clasica consola GameBoy original, con la que podras disfrutar juegos de epoca'
 });
 
 productList.push({
     name: 'TOYOTA MR2',
     price: 7000,
-    imagen: 'https://images.pexels.com/photos/17797237/pexels-photo-17797237/free-photo-of-coche-vehiculo-vintage-retro.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
-
+    imagen: 'https://images.pexels.com/photos/17797237/pexels-photo-17797237/free-photo-of-coche-vehiculo-vintage-retro.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+    descripcion: 'Arto auto que nos hace recordar a peliculas de los 80s',
 })
 
 //
 productList.push({
     name: 'Cassete Video',
     price: 80,
-    imagen: 'https://images.pexels.com/photos/17758160/pexels-photo-17758160/free-photo-of-sucio-vintage-retro-kodak.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+    imagen: 'https://images.pexels.com/photos/17758160/pexels-photo-17758160/free-photo-of-sucio-vintage-retro-kodak.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    descripcion: 'Sinta de video VHS',
 })
 
 //6
 productList.push({
     name: 'Telefono',
     price: 120,
-    imagen: 'https://images.pexels.com/photos/17786712/pexels-photo-17786712/free-photo-of-madera-vintage-de-madera-antiguo.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+    imagen: 'https://images.pexels.com/photos/17786712/pexels-photo-17786712/free-photo-of-madera-vintage-de-madera-antiguo.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    descripcion: 'Un clasico teléfono funcional, con roldete de marcas',
 
 })
 
@@ -136,7 +142,8 @@ productList.push({
 productList.push({
     name: 'Wold Waggen Band',
     price: 7000,
-    imagen: 'https://images.pexels.com/photos/17606859/pexels-photo-17606859/free-photo-of-vehiculo-viaje-viajar-retro.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+    imagen: 'https://images.pexels.com/photos/17606859/pexels-photo-17606859/free-photo-of-vehiculo-viaje-viajar-retro.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    descripcion: 'Mini Band clasica de los años 60',
 
 })
 
@@ -144,7 +151,8 @@ productList.push({
 productList.push({
     name: 'Motor - V5 ',
     price: 7000,
-    imagen: 'https://images.pexels.com/photos/17575475/pexels-photo-17575475/free-photo-of-coche-vehiculo-tuberias-retro.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+    imagen: 'https://images.pexels.com/photos/17575475/pexels-photo-17575475/free-photo-of-coche-vehiculo-tuberias-retro.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    descripcion: 'Un maquinon, no te deja tirado con tremendo motor. Como nafta como loco',
 
 })
 
@@ -152,7 +160,8 @@ productList.push({
 productList.push({
     name: 'Write Machine',
     price: 600,
-    imagen: 'https://images.pexels.com/photos/3808904/pexels-photo-3808904.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+    imagen: 'https://images.pexels.com/photos/3808904/pexels-photo-3808904.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    descripcion: 'Maquina de escribir de Isaac Assimov, en ella escribio sus mejores novelas de ciencia ficción',
 
 })
 
@@ -174,7 +183,7 @@ function renderProducts(arr){
 
         //Agregamos el addEventListener aqui por que en HTML no existen estos elementos, aca si por que lo creamos por JS
         img.addEventListener('click', function(event) {
-            
+
             //Llamamos a la función para remover la clase "inactive" y visualizar el Aside
             openProductDetailAside();
             
@@ -199,7 +208,12 @@ function renderProducts(arr){
             });
             priceDetail.innerText = '$' + articuloFiltradoPrecio;
 
-          
+            //Ontenemos la descripción 
+            const descripcionFiltrado = articuloFiltrado.map(function(nameFilter){
+                return nameFilter.descripcion
+            });
+            descripcionDetail.innerHTML = descripcionFiltrado;
+
         });
 
     
